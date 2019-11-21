14aa039f5a82bdc7d13967736b55c717e63e3e10
gongdaocai <18371800703@163.com>
2019-11-21 08:49:47 +0800

    尺寸

--- src/main/java/com/xcly/util/DateUtil.java
+++ src/main/java/com/xcly/util/DateUtil.java
@@ -6,7 +6,14 @@ import java.util.Date;
 public class DateUtil {
     private final static String YEAR_MONTH_DAY_HOUR_MINUTE_SECONDS = "yyyy-MM-dd HH:mm:ss";
 
+    /**
+     *
+     * @param date 时间
+     * @param reg 格式
+     * @return
+     */
     public static String format1(Date date, String reg) {
+        //定义一个格式
         SimpleDateFormat format = new SimpleDateFormat(reg == null ? YEAR_MONTH_DAY_HOUR_MINUTE_SECONDS : reg);
 
         return format.format(date);
